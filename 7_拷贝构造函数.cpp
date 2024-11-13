e890f2b285845130f0f7bfbfe5bac9bdde3d5541
User <2085163736@qq.com>
2024-11-13 20:15:30 +0800

    change

--- 7_拷贝构造函数.cpp
+++ 7_拷贝构造函数.cpp
@@ -29,15 +29,16 @@ public:
 };
 int main()
 {
+    person p0();    //只写空括号 不会成功创建对象，编译器会认为这是函数的声明 
     person p(10,"ZhangSan");
     person p2(p);   //括号法    
  
+    p2 = person(20,"LiSi"); //显示法
 
-
+    person(10,"no_name");   //匿名对象 可以用‘=’给他一个名 (显示法)
     cout<<p._age<<p._name<<endl;
     cout<<p2._age<<p2._name<<endl;
-
-
-
+    //person(p2);//编译器会认为 person(p3) === person p3 编译器会认为这是一个对象的声明。
+    person p4 = {10,"name"};
 
 }
