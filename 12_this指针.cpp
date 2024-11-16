621cf70858b35b85df98aca797a870ae185338cc
User <2085163736@qq.com>
2024-11-16 13:29:42 +0800

    update files

--- 12_this指针.cpp
+++ 12_this指针.cpp
@@ -8,8 +8,8 @@ public:
         this->age = age;
         
     }
-    person returning()
-    {
+    person& returning()    //如果不返回引用，则会返回一个对象的拷贝
+    {                       //相当于重新创建了一个 “值” 对象 而这个对象是不可以作左值的
         return *this;
     }
     int age;
@@ -19,6 +19,7 @@ int main()
 {
     person p1(18);
     cout<<p1.age<<endl;
+    p1.returning().age = 20;
     cout<<p1.returning().age;
 
 
