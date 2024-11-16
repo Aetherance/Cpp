b5ee07cae704b34aa2eb00159997903119c89e03
User <2085163736@qq.com>
2024-11-16 09:23:20 +0800

    update

--- 10_静态成员.cpp
+++ 10_静态成员.cpp
@@ -15,6 +15,18 @@ public:
         cout<<"析构函数调用"<<endl;
     }//如果将构造函数私有化，就无法创建对象了
 };
+class personp
+{
+public:
+    string name = "name";
+    static int num;
+    static void func()
+    {
+        cout<<"STATIC func 调用";
+        num = 200;
+        //name = "123";
+    }
+};
 int person::num = 100;
 int main()
 {
