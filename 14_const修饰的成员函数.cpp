eb58423f56bdc347af2afeb44d28a947c65082ac
User <2085163736@qq.com>
2024-11-16 14:42:22 +0800

    update files

--- 14_const修饰的成员函数.cpp
+++ 14_const修饰的成员函数.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 using namespace std;
+//常函数和常对象
+class person
+{
+public:
+//this指针的本质是指针常量 他的指向不可修改
+    void showPerson() const     //加上这个const后，会使this指向的值也不可修改
+    {
+        b = 100;
+
+        //this = NULL;  this 的指向是不可修改的
+    }
+
+mutable int b;//特殊变量，即使在常函数中也可修改
+int a;
+};
 int main()
 {
-    
+    const person p = person();
 
 
 
