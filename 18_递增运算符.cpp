44399fd8bbd475231bc86a5daaaa8ea5a70febe4
User <2085163736@qq.com>
2024-12-01 17:44:41 +0800

    update files

--- 18_递增运算符.cpp
+++ 18_递增运算符.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class my_integer
 {
-friend ostream& operator<<(ostream& cout,my_integer &myint);
+friend ostream& operator<<(ostream& cout,my_integer _myint);
 public:
 
     my_integer()
@@ -16,18 +16,21 @@ public:
         num++;
         return *this;   //返回引用是为了一直对一个数据进行递增
     }
-    my_integer& operator++(int)     //在这里加一个int占位 表示这是一个后置递增
+    my_integer operator++(int)     //在这里加一个int占位 表示这是一个后置递增
     {                               //只能是int
-
+        //先记录当时结果，再递增，最后返回当时结果
+        my_integer temp = *this;
+        num ++;
+        return temp;    //这里返回的是一个值，而不是一个变量
     }
 
 private:
     
     int num;
 };
-ostream& operator<<(ostream&cout,my_integer &myint)
+ostream& operator<<(ostream&cout,my_integer _myint)
 {
-    cout<<myint.num;
+    cout<<_myint;
     return cout;
 }
 
@@ -41,13 +44,10 @@ int main()
 {
     int a = 0;
     my_integer myint;
-    cout<<++++myint<<endl;
-    
-
-
-
-
-
+    cout<<(++++myint)<<endl;
+    my_integer my = myint++;
+    cout<<myint++<<endl;
+    cout<<myint<<endl;
 
 
 
