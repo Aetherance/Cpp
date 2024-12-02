7e631f3b94d4bb2bdc57afc2b4584c3fe13484c9
User <2085163736@qq.com>
2024-12-02 20:28:39 +0800

    update 25

---
+++ 25_继承_构造和析构顺序.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+    Base()
+    {
+        cout<<"Base 的 构造函数调用"<<endl;
+    }
+
+    ~Base()
+    {
+        cout<<"Base 的 析构函数调用"<<endl;
+    }
+
+};
+
+class son
+{
+
+
+}
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
