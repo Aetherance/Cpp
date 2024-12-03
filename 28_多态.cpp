1fa6881d76d8601721008fbea7050b50721a3886
User <2085163736@qq.com>
2024-12-03 13:04:34 +0800

    update files

--- 28_多态.cpp
+++ 28_多态.cpp
@@ -1,9 +1,40 @@
 #include<iostream>
 using namespace std;
+
+class Animal
+{
+public:
+    virtual void speak()
+    {
+        cout<<"动物在说话!"<<endl;
+    }
+
+
+};
+class Cat : public Animal
+{
+public:
+    void speak()
+    {
+        cout<<"Cat speaking"<<endl;
+    }
+
+
+
+};
+
+void doSpeak(Animal & ani)
+{
+    ani.speak(); // 地址早绑定 已经绑定到了animal的地址，所有不管传什么都调用animal的成员函数
+
+
+}
+
 int main()
 {
-    
-
+    Cat cat;
+    cat.speak();
+    doSpeak(cat);
 
 
 
