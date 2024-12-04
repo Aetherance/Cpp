2e18a1c236d17747a982835f4b4bc14db2d4c28d
User <2085163736@qq.com>
2024-12-04 13:38:40 +0800

    update map.cpp

---
+++ map容器.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+int main()
+{
+    map<int,int> m;
+    m.insert(pair(10,20));
+    m.insert(pair(12,20));
+    m.insert(pair(11,20));
+    m.insert(pair(15,20));
+
+    for(map<int,int>::iterator it = m.begin();it!=m.end();it++)
+        cout<<(*it).first<<" "<<it->second <<endl;
+
+
+
+    return 0;
+}
